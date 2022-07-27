1383b2f0817c0bf53606113a3f07c09d3f03b53a
[Enes Akturk] <[enesakturk999@hotmail.com]>
2022-07-27 12:36:01 +0300

    teleport, slot, components, click events, methods, emit are learned

---
+++ Vue-04/modal-project/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <h1>{{ title }}</h1>
+  <!--
+  <input type="text" ref="name">
+  <button @click="handleClick">Click Me</button>
+  -->
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme='sales' @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>{{header}}</h1>
+      <p>{{text}}</p>
+    </Modal>
+  </teleport>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1 href="#">Sign up for free newsletter</h1>
+      <a href="#">More info</a>
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal 2</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: 'My First Vue App',
+      header: 'Sign up for the giveaway!',
+      text: 'Grab your phone %20 off',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    /*handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }*/
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    }
+  }
+}
+</script>
+
+<style>
+#app, .modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px; 
+}
+</style>
